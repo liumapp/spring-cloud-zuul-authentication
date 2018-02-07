d725757a51b9e7534ce563e99403c7c8ed8e1a84
liumapp <liumapp.com@gmail.com>
2018-02-07 14:55:59 +0800

    add mapper and domain for sql

---
+++ zuul-auth-gateway/src/main/java/com/liumapp/auth/zuul/gateway/config/SecurityConf.java
@@ -0,0 +1,9 @@
+package com.liumapp.auth.zuul.gateway.config;
+
+/**
+ * Created by liumapp on 2/7/18.
+ * E-mail:liumapp.com@gmail.com
+ * home-page:http://www.liumapp.com
+ */
+public class SecurityConf {
+}
