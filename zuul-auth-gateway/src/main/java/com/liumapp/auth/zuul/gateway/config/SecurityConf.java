30df44e42957e7a340cc072ca3fab0bb567a90e9
liumapp <liumapp.com@gmail.com>
2018-02-07 14:58:07 +0800

    add necessary dependency

--- zuul-auth-gateway/src/main/java/com/liumapp/auth/zuul/gateway/config/SecurityConf.java
+++ zuul-auth-gateway/src/main/java/com/liumapp/auth/zuul/gateway/config/SecurityConf.java
@@ -1,9 +1,12 @@
 package com.liumapp.auth.zuul.gateway.config;
 
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Created by liumapp on 2/7/18.
  * E-mail:liumapp.com@gmail.com
  * home-page:http://www.liumapp.com
  */
+@Configuration
 public class SecurityConf {
 }
