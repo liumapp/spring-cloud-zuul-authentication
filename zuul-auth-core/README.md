04c8232136dfcd0441c7e13fa1aff1c45db1b40f
liumapp <liumapp.com@gmail.com>
2018-02-08 15:33:01 +0800

    add core for auth

---
+++ zuul-auth-core/README.md
@@ -0,0 +1,10 @@
+# spring-boot-util-module
+A lib module for SpringBoot , can not run alone . 
+
+## documents
+
+* mvn archetype:create-from-project
+
+* cd target/generated-sources/archetype
+
+* mvn install
---
+++ zuul-auth-core/src/main/java/com/liumapp/auth/zuul/core/Main.java
@@ -0,0 +1,17 @@
+package com.liumapp.auth.zuul.core;
+
+import com.liumapp.auth.zuul.core.config.UtilConfig;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * Created by liumapp on 9/28/17.
+ * E-mail:liumapp.com@gmail.com
+ * home-page:http://www.liumapp.com
+ */
+@Configuration
+@Import({UtilConfig.class})
+public class Main {
+
+
+}
