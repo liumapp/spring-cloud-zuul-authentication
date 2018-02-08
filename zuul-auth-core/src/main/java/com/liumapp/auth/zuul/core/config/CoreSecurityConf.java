5de368a3fb389e5cb4346a916786f069388af03d
liumapp <liumapp.com@gmail.com>
2018-02-08 15:46:56 +0800

    add spring security dependency for core

---
+++ zuul-auth-core/src/main/java/com/liumapp/auth/zuul/core/config/CoreSecurityConf.java
@@ -0,0 +1,20 @@
+package com.liumapp.auth.zuul.core.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+/**
+ * Created by liumapp on 2/8/18.
+ * E-mail:liumapp.com@gmail.com
+ * home-page:http://www.liumapp.com
+ */
+@SuppressWarnings("SpringJavaAutowiringInspection")
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class CoreSecurityConf {
+
+    
+
+}
