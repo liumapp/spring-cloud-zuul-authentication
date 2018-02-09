599333b3588a645921ab372596556a8f6385bb34
liumapp <liumapp.com@gmail.com>
2018-02-09 16:25:07 +0800

    rm unnecessary package

--- zuul-auth-token-manager/src/main/java/com/liumapp/auth/zuul/token/controller/AuthController.java
+++ zuul-auth-token-manager/src/main/java/com/liumapp/auth/zuul/token/controller/AuthController.java
@@ -15,7 +15,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
