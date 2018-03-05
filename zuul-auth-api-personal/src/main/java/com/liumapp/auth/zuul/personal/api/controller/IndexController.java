508def004441920c3ccd07519aa455fc14ed7621
liumapp <liumapp.com@gmail.com>
2018-03-05 14:48:50 +0800

    add active info in personal controller

--- zuul-auth-api-personal/src/main/java/com/liumapp/auth/zuul/personal/api/controller/IndexController.java
+++ zuul-auth-api-personal/src/main/java/com/liumapp/auth/zuul/personal/api/controller/IndexController.java
@@ -1,11 +1,13 @@
 package com.liumapp.auth.zuul.personal.api.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Created by liumapp on 9/28/17.
+ * Created by liumapp on 3/5/18 2:48 PM.
  * E-mail:liumapp.com@gmail.com
  * home-page:http://www.liumapp.com
  */
@@ -13,9 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/personal")
 public class IndexController {
 
+    @Value("${custom.activeInfo}")
+    private String activeInfo;
+
     @RequestMapping("/")
     public String index (ModelMap model) {
         return "Hello , this is Personal api demo ";
+    }
+
+    @RequestMapping("/hello")
+    public ResponseEntity<?> hello () {
+        return ResponseEntity.ok("hello , and active info is : " + activeInfo);
     }
 
 }
